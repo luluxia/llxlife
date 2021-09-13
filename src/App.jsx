5a4f69dd2a127936d15b1c7627fc80aa24435acc
luluxia <306924625@qq.com>
2021-09-13 21:34:57 +0800

    分离噪音组件，去除无用代码

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useRef, useContext } from 'react'
+import Grain from './card/Grain'
 import _ from 'lodash'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
@@ -7,108 +8,6 @@ import './App.sass'
 dayjs.locale('zh-cn')
 dayjs.extend(relativeTime)
 const Context = React.createContext({})
-// 噪音
-function Grain() {
-  class Grain {
-    constructor (el) {
-        /**
-     * Options
-     * Increase the pattern size if visible pattern
-     */
-        this.patternSize = 150;
-        this.patternScaleX = 1;
-        this.patternScaleY = 1;
-        this.patternRefreshInterval = 3; // 8
-        this.patternAlpha = 15; // int between 0 and 255,
-  
-        /**
-     * Create canvas
-     */
-        this.canvas = el;
-        this.ctx = this.canvas.getContext('2d');
-        this.ctx.scale(this.patternScaleX, this.patternScaleY);
-  
-        /**
-     * Create a canvas that will be used to generate grain and used as a
-     * pattern on the main canvas.
-     */
-        this.patternCanvas = document.createElement('canvas');
-        this.patternCanvas.width = this.patternSize;
-        this.patternCanvas.height = this.patternSize;
-        this.patternCtx = this.patternCanvas.getContext('2d');
-        this.patternData = this.patternCtx.createImageData(this.patternSize, this.patternSize);
-        this.patternPixelDataLength = this.patternSize * this.patternSize * 4; // rgba = 4
-  
-        /**
-     * Prebind prototype function, so later its easier to user
-     */
-        this.resize = this.resize.bind(this);
-        this.loop = this.loop.bind(this);
-  
-        this.frame = 0;
-  
-        window.addEventListener('resize', this.resize);
-        this.resize();
-  
-        window.requestAnimationFrame(this.loop);
-    }
-  
-    resize () {
-        this.canvas.width = window.innerWidth * devicePixelRatio;
-        this.canvas.height = window.innerHeight * devicePixelRatio;
-    }
-  
-    update () {
-        const {patternPixelDataLength, patternData, patternAlpha, patternCtx} = this;
-  
-        // put a random shade of gray into every pixel of the pattern
-        for (let i = 0; i < patternPixelDataLength; i += 4) {
-            // const value = (Math.random() * 255) | 0;
-            const value = Math.random() * 255;
-  
-            patternData.data[i] = value;
-            patternData.data[i + 1] = value;
-            patternData.data[i + 2] = value;
-            patternData.data[i + 3] = patternAlpha;
-        }
-  
-        patternCtx.putImageData(patternData, 0, 0);
-    }
-  
-    draw () {
-        const {ctx, patternCanvas, canvas, viewHeight} = this;
-        const {width, height} = canvas;
-  
-        // clear canvas
-        ctx.clearRect(0, 0, width, height);
-  
-        // fill the canvas using the pattern
-        ctx.fillStyle = ctx.createPattern(patternCanvas, 'repeat');
-        ctx.fillRect(0, 0, width, height);
-    }
-  
-    loop () {
-        // only update grain every n frames
-        const shouldDraw = ++this.frame % this.patternRefreshInterval === 0;
-        if (shouldDraw) {
-            this.update();
-            this.draw();
-        }
-  
-        window.requestAnimationFrame(this.loop);
-    }
-  }
-  useEffect(() => {
-    /**
-    * Initiate Grain
-    */
-    const el = document.querySelector('.grain');
-    const grain = new Grain(el);
-  }, [])
-  return (
-    <canvas className="grain"></canvas>
-  )
-}
 function Flag() {
   return (
     <div className="flag">
@@ -586,12 +485,6 @@ function App() {
                   key={index}
                   onClick={e => {openCard(e)}}
                 >
-                  { className == 'painting' &&
-                    <img src={`img/painting${Math.floor(Math.random() * 6 + 1)}.png`} alt=""/>
-                  }
-                  { className == 'pokemon' &&
-                    <img src={`https://cn.portal-pokemon.com/play/resources/pokedex${data.pokemon[id]}`} alt=""/>
-                  }
                   { className == 'logo' && <Logo/> }
                   { className == 'about' && <About/> }
                   { className == 'link' && <Link/> }
