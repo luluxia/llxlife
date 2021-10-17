21127bef656d1e748a391c8ceae7ac199a268fb2
luluxia <306924625@qq.com>
2021-10-17 16:28:32 +0800

    完善相册

--- src/box/Photo.jsx
+++ src/box/Photo.jsx
@@ -1,8 +1,22 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useQuery, gql } from "@apollo/client"
+import { useParams } from "react-router"
 function Photo(props) {
+  const { id } = useParams()
+  const { data } = useQuery(gql`
+    query GetPhoto($id: Int) {
+      llxlife_photo(where: {id: {_eq: $id}}) {
+        img
+      }
+    }
+  `, {
+    variables: { id: id }
+  })
   return (
     <article className="photo">
-      <img src="../img/test.jpg" alt="" />
+      {
+        data && <img src={data.llxlife_photo[0].img} alt="" />
+      }
     </article>
   )
 }
--- src/box/PhotoList.jsx
+++ src/box/PhotoList.jsx
@@ -1,33 +1,59 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useQuery, gql } from "@apollo/client"
 import { useHistory } from "react-router"
 function PhotoList(props) {
   const history = useHistory()
+  const [finish, setFinish] = useState(0)
+  const { data, fetchMore } = useQuery(gql`
+    query GetPhotos($offset: Int) {
+      llxlife_photo(limit: 10, offset: $offset) {
+        id
+        time
+        title
+        tag
+        img
+      }
+      llxlife_photo_aggregate {
+        aggregate {
+          count
+        }
+      }
+    }
+  `, {
+    variables: { offset: 0 }
+  })
+  function loadMore() {
+    fetchMore({
+      variables: {
+        offset: data.llxlife_photo.length
+      }
+    })
+  }
+  useEffect(() => {
+    if (data) {
+      if (data.llxlife_photo.length == data.llxlife_photo_aggregate.aggregate.count) {
+        setFinish(1)
+      }
+    }
+  }, [data])
   return (
     <>
       <h1 className="box-title">相册</h1>
       <div className="article-list">
-        <div onClick={() => {props.openSub();history.push('/photo/1' + 1)}} className="article-item photo-item">
-          <img src="../img/test.jpg" alt="" />
-          <div className="photo-info">
-            <p className="article-time">2021-8-8</p>
-            <p className="article-title">关于陆陆侠的生活</p>
-            <p className="article-tag">作品诞生记</p>
-          </div>
-        </div>
-        <div className="article-item photo-item">
-          <p className="article-time">2021-8-8</p>
-          <p className="article-title">关于陆陆侠的生活</p>
-          <p className="article-tag">作品诞生记</p>
-        </div>
-        <div className="article-item photo-item">
-          <p className="article-time">2021-8-8</p>
-          <p className="article-title">关于陆陆侠的生活</p>
-          <p className="article-tag">作品诞生记</p>
-        </div>
-        <div className="article-item photo-item">
-          <p className="article-time">2021-8-8</p>
-          <p className="article-title">关于陆陆侠的生活</p>
-          <p className="article-tag">作品诞生记</p>
+        { data &&
+          data.llxlife_photo.map(item => (
+            <div key={item.id} onClick={() => {props.openSub();history.push('/photo/' + item.id)}} className="article-item photo-item">
+              <img src={item.img} alt="" />
+              <div className="photo-info">
+                <p className="article-time">{new Date(item.time).toLocaleString('cn-ZH', { hour12: false })}</p>
+                <p className="article-title">{item.title}</p>
+                <p className="article-tag">{item.tag}</p>
+              </div>
+            </div>
+          ))
+        }
+        <div onClick={() => {loadMore()}} className={`article-item load-more ${finish ? 'load-finish' : ''}`}>
+          {finish ? '已达岁月尽头' : '加载更多'}
         </div>
       </div>
     </>
